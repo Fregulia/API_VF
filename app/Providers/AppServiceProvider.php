4e7b05d45eb51abab7fbedbd7c1d576027ae4ef4
Vitor Fregulia <vitorfregulia2005@gmail.com>
2025-07-15 10:59:56 -0300

    API - Deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,12 +18,22 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        //Configura limite de requisições para a API
+        $this->configureRateLimiting();
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
+    //Para limitar requisições na API por usuário ou IP
+    protected function configureRateLimiting()
+    {
+        RateLimiter::for('api', function (Request $request) {
+            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
     }
 }
